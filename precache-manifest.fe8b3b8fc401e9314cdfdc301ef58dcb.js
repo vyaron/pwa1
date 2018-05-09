29b7b821a38197ea2f7dde6b4fb5ff43660b51f5
vyaron <vyaron@gmail.com>
2018-05-09 12:10:22 +0300

    v4

---
+++ precache-manifest.fe8b3b8fc401e9314cdfdc301ef58dcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a114a901ad3e7fc2b0e3",
+    "url": "/js/vendor.a114a901.js"
+  },
+  {
+    "revision": "bb41d6d80c1fdaad6874",
+    "url": "/js/manifest.bb41d6d8.js"
+  },
+  {
+    "revision": "200091543ac930aae657",
+    "url": "/js/app.20009154.js"
+  },
+  {
+    "revision": "45b33bc853c8a44906efe5aacdb85d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "200091543ac930aae657",
+    "url": "/css/app.cf9f195d.css"
+  }
+];
